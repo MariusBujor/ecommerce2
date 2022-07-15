cfa3ed5a8ef5c49436e1d6bb31a373cec2962229
Marius Corneliu Bujor <bujormarius1982@gmail.com>
2022-07-15 19:37:33 +0000

    Added form in forms.py and checked if the data is received in terminal by submiting

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,14 +1,36 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import ContactForm
+
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {"title": " New books have arrived !!",
+               "content": " Welcome to the home page"}
+    return render(request, "home_page.html", context)
+
 
 def about_page(request):
-    return render(request, "home_page.html", {})
+    context = {"title": " About Page ",
+               "content": " Welcome to the about page"}
+    return render(request, "home_page.html", context)
+
 
 def contact_page(request):
-    return render(request, "home_page.html", {})
+    contact_form = ContactForm(request.POST or None)
+    context = {"title": " Contact page ",
+               "content": " Welcome to the contact page",
+               "form": contact_form,
+               }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+
+    # if request.method == "POST":
+    #     # print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
+
+    return render(request, "contact/view.html", context)
 
 
 """def home_page_old(request):
